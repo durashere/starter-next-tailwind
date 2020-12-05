7475a0a1060c213ea0aaf34103a1219cec93f601
Krzysztof Durek <walendes@gmail.com>
2020-12-05 11:07:12 +0100

    back to starter

--- pages/index.js
+++ pages/index.js
@@ -4,14 +4,14 @@ export default function Home() {
   return (
     <div className="">
       <Head>
-        <title>Todo</title>
+        <title>Starter Next Tailwind</title>
       </Head>
 
       <main className=""></main>
 
       <footer className="">
         <p className="pb-4 font-mont text-gray-300 text-sm font-semibold">
-          <a href="https://github.com/durashere">durashere</a> @{" "}
+          <a href="https://github.com/{your_nickname}">your_nickname</a> @{" "}
           <a href="https://devchallenges.io">DevChallenges.io</a>
         </p>
       </footer>
