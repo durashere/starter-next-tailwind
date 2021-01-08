53dc2be5ca28abe43a6ffb4ab93716e270f78695
Krzysztof Durek <walendes@gmail.com>
2021-01-08 10:48:32 +0100

    updated tailwind colors

--- src/components/modules/Footer.js
+++ src/components/modules/Footer.js
@@ -1,6 +1,6 @@
 const Footer = () => {
   return (
-    <footer className="pb-4 text-sm font-semibold text-black font-mon">
+    <footer className="pb-4 text-sm font-semibold text-blueGray-300 font-mon">
       Made by{' '}
       <a href="https://github.com/durashere" target="_blank" rel="noreferrer">
         durashere
